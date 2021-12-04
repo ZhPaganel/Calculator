8f59001096ce4d50345a555736a5ad47bc6f82d7
ZhPaganel <anechaj2791@gmail.com>
2021-12-04 18:12:56 +0200

    writed code

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-#print ("Hello world")
+#code starts here
+x = input()
+y = x.split(' ')
+num = []
+oper = []
+for i in range(len(y)):
+    if i % 2 == 0:
+        num.append(float(y[i]))
+    else:
+        oper.append(y[i])
+print (oper[0])
+for j in range(len(oper)):
+    if (oper[j]=='+'):
+        num[0] = num[0] + num[1]
+        del(num[1])
+    if (oper[j]=='-'):
+        num[0] = num[0] - num[1]
+        del(num[1])
+    if (oper[j]=='*'):
+        num[0] = num[0] * num[1]
+        del(num[1])
+    if (oper[j]=='/'):
+        num[0] = num[0] / num[1]
+        del(num[1])
+
+
+if num[0] % 1 == 0:
+    num[0] = int(num[0])
+    
+print ("Result: ", num[0])
